83c285faa8e690902c67da6298da59416d35b582
RoySamson-stack <rsam9009@gmail.com>
2021-11-07 22:19:57 +0300

    adding the producst for the prodcust page

--- src/components/products.js
+++ src/components/products.js
@@ -3,11 +3,17 @@
 function Products({image, name, description, price}){
  return(
   <div class="">
-  <img src={image} alt="product image" />
-  <h3>{name}</h3>
-  <p>{description}</p>
-  <p>{price}</p>  
-</div>
+   <div class="bg-gray-400 w-1/3 m-14 text-center">
+    <div class="card-image p-2">
+     <img src={image}/>
+    </div>
+    <div class="">
+     <p>{name}</p>
+     <p>{description}</p>
+     <p>{price}</p>
+    </div>
+   </div>
+  </div>
  );
 }
 export default Products;
--- src/components/store.js
+++ src/components/store.js
@@ -1,14 +1,32 @@
 import Product from './products';
+import tv from  "../img/tv.png"
 
 
 function Store(){
   return(
     <div>
+      <div class="grid">
       <Product 
-       image="image"
+       image={tv}
        name="image"
        description="image"
        price="image"/>
+       <Product 
+       image={tv}
+       name="image"
+       description="image"
+       price="image"/>
+       <Product 
+       image={tv}
+       name="image"
+       description="image"
+       price="image"/>
+       <Product 
+       image={tv}
+       name="image"
+       description="image"
+       price="image"/>
+       </div>
     </div>
   );
 }
